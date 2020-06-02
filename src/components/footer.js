a587d827ccadd7f4fe9d5b69b2aa0754910ac6a8
Jonathan Giardino <j.giardino@me.com>
2020-06-02 09:39:09 +0200

    Add new links to external profiles

--- src/components/footer.js
+++ src/components/footer.js
@@ -13,6 +13,8 @@ const FooterContainer = styled.div`
   justify-content: center;
   align-items: center;
   @media ${props => props.theme.breakpoints.mobile} {
+    justify-content: space-evenly;
+    padding: 1.5rem 0;
   }
 `
 
@@ -20,6 +22,10 @@ const ImageWrapper = styled.a`
   text-decoration: none;
   padding: 1rem;
   margin: 1rem;
+  @media ${props => props.theme.breakpoints.mobile} {
+    padding: 0.3rem;
+    margin: 0.3rem;
+  }
 `
 
 const Footer = props => {
@@ -29,21 +35,49 @@ const Footer = props => {
     query {
       githubBlack: file(relativePath: { eq: "github-black.png" }) {
         childImageSharp {
-          fixed(width: 40) {
+          fixed(width: 35) {
             ...GatsbyImageSharpFixed
           }
         }
       }
       githubWhite: file(relativePath: { eq: "github-white.png" }) {
         childImageSharp {
-          fixed(width: 40) {
+          fixed(width: 35) {
             ...GatsbyImageSharpFixed
           }
         }
       }
       webflow: file(relativePath: { eq: "webflow.png" }) {
         childImageSharp {
-          fixed(width: 40) {
+          fixed(width: 35) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      medium: file(relativePath: { eq: "medium.png" }) {
+        childImageSharp {
+          fixed(width: 35) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      devWhite: file(relativePath: { eq: "devWhite.png" }) {
+        childImageSharp {
+          fixed(width: 35) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      dev: file(relativePath: { eq: "dev.png" }) {
+        childImageSharp {
+          fixed(width: 35) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      dribbble: file(relativePath: { eq: "dribbble.png" }) {
+        childImageSharp {
+          fixed(width: 35) {
             ...GatsbyImageSharpFixed
           }
         }
@@ -64,6 +98,24 @@ const Footer = props => {
       </ImageWrapper>
       <ImageWrapper href="https://webflow.com/jonathangiardino" target="_blank">
         <Img fixed={img.webflow.childImageSharp.fixed} />
+      </ImageWrapper>
+      <ImageWrapper href="https://medium.com/@jonathangiardino" target="_blank">
+        <Img fixed={img.medium.childImageSharp.fixed} />
+      </ImageWrapper>
+      <ImageWrapper href="https://dev.to/jonathan_gardn" target="_blank">
+        <Img
+          fixed={
+            state.isDark
+              ? img.devWhite.childImageSharp.fixed
+              : img.dev.childImageSharp.fixed
+          }
+        />
+      </ImageWrapper>
+      <ImageWrapper
+        href="https://dribbble.com/jonathangiardino"
+        target="_blank"
+      >
+        <Img fixed={img.dribbble.childImageSharp.fixed} />
       </ImageWrapper>
     </FooterContainer>
   )
